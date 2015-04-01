ea9c700e3bb11a00286c74e01a6adf45fa810826
Alisson Barbosa Ferreira <alisson.barbosa@pgplayer.com.br>
2015-04-01 13:04:24 -0300

    Adicionado o projeto ao Read The Docs

--- docs/conf.py
+++ docs/conf.py
@@ -22,9 +22,9 @@ import shlex
 #sys.path.insert(0, os.path.abspath('.'))
 
 
-sys.path.insert(0, os.path.abspath('..'))
-from django.conf import settings
-settings.configure()
+#sys.path.insert(0, os.path.abspath('..'))
+#from django.conf import settings
+#settings.configure()
 
 
 # -- General configuration ------------------------------------------------
